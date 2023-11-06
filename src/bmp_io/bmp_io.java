e7791288a5f3763fa9f03f88249bd9d25149646c
jonnijoe1990 <72992310+jonnijoe1990@users.noreply.github.com>
2023-11-06 13:41:44 +0100

    s

--- src/bmp_io/bmp_io.java
+++ src/bmp_io/bmp_io.java
@@ -31,31 +31,11 @@ public final class bmp_io {
 		
 		// ****************************************************
 		// Implementierung bei einem Eingabeparamter
-		
 		inFilename = args[0];
 		
 		try {
 			InputStream in = new FileInputStream(inFilename);
 			bmp = BmpReader.read_bmp(in);
-			
-			// Implementierung
-			width = bmp.image.getWidth(); 
-			height = bmp.image.getHeight(); 
-			
-			for (int x = 0; x != width; x++) 
-				for (int y = 0; y != height; y++) {
-					// Zugriff auf Pixel mit bmp.image.getRgbPixel(x, y);
-					PixelColor pOut = bmp.image.getRgbPixel(x, y);
-					System.out.printf("R: %-3d ", pOut.r);
-					System.out.printf("G: %-3d ", pOut.g);
-					System.out.printf("B: %-3d ", pOut.b); 
-					System.out.println(); 
-				}
-			
-			// Setzen eines Pixels mit bmp.image.setRgbPixel(x, y, pc);
-				
-			
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -64,16 +44,12 @@ public final class bmp_io {
 		if (args.length == 1) 
 			System.exit(0);
 	
-		
 		// ***************************************************
 	    // Implementierung bei Ein- und Ausgabeparameter (speichern in eine Datei (2. Argument))
 
 		outFilename = args[1];
 		OutputStream out = new FileOutputStream(outFilename);
-		
-		// Implementierung
-		
-		
+
 		// Speicherung 
 		try {
 			BmpWriter.write_bmp(out, bmp);

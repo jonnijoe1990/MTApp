45e168818f67ba1f66a50209b911ad32cc3ebc06
jonnijoe1990 <72992310+jonnijoe1990@users.noreply.github.com>
2023-11-13 15:19:01 +0100

    Revert "Revert "s""
    This reverts commit e651b8a87655bf6f6282c9619b7f2308d7411c6c.

---
+++ src/bmp_io/_ue31.java
@@ -0,0 +1,83 @@
+package bmp_io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public final class _ue31 {
+	
+	public static void main(String[] args) throws IOException {
+		BmpImage horizontalBmp = null; 
+		BmpImage verticalBmp = null; 	
+
+		String horizontalFile = "grating_HX";
+		String verticalFile = "grating_VX"; 
+		
+		try {
+			// try to get bmpImage objects from both bmp files 
+			InputStream horizontalIn = new FileInputStream(horizontalFile + ".bmp");
+			InputStream verticalIn = new FileInputStream(horizontalFile + ".bmp");
+			horizontalBmp = BmpReader.read_bmp(horizontalIn); 
+			verticalBmp = BmpReader.read_bmp(verticalIn); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		OutputStream horizontalOut = new FileOutputStream(horizontalFile + ".ascii");
+		OutputStream verticalOut = new FileOutputStream(verticalFile + ".ascii");
+
+		// create data strings for both files
+		String horizontalDataString = ""; 
+		String verticalDataString = ""; 
+		
+		try {
+			// horizontal
+			PixelColor[][] horizontalPixels = new PixelColor[horizontalBmp.image.getWidth()][horizontalBmp.image.getHeight()]; 
+			for (int x = 0; x != horizontalBmp.image.getWidth(); x++) {
+				for (int y = 0; y != horizontalBmp.image.getHeight(); y++) {
+					// set pixel data 
+					horizontalPixels[x][y] = horizontalBmp.image.getRgbPixel(x, y);
+					// set horizontal data String for first row
+					if (y == 0) {
+						horizontalDataString += horizontalPixels[x][y].r + " " + horizontalPixels[x][y].g + " " + horizontalPixels[x][y].b;
+						if (x < horizontalBmp.image.getWidth() - 1) {
+							// append new line, except for last entry
+							horizontalDataString += "\n";
+						}
+					} 
+				}
+			}
+			horizontalOut.write(horizontalDataString.getBytes()); // write data 
+			horizontalOut.flush(); // ensure file is fully written 
+
+			// vertical
+			PixelColor[][] verticalPixels = new PixelColor[verticalBmp.image.getWidth()][verticalBmp.image.getHeight()]; 
+			for (int x = 0; x != verticalBmp.image.getWidth(); x++) {
+				for (int y = 0; y != verticalBmp.image.getHeight(); y++) {
+					// set pixel data 
+					verticalPixels[x][y] = verticalBmp.image.getRgbPixel(x, y);
+					// set vertical data String for first col
+					if (x == 0) {
+						verticalDataString += verticalPixels[x][y].r + " " + verticalPixels[x][y].g + " " + verticalPixels[x][y].b;
+						if (y < verticalBmp.image.getHeight() - 1) {
+							// append new line, except for last entry
+							verticalDataString += "\n";
+						}
+					} 
+				}
+			}
+			verticalOut.write(verticalDataString.getBytes()); // write data 
+			verticalOut.flush(); // ensure file is fully written 
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// close output streams
+			horizontalOut.close();
+			verticalOut.close();
+		}
+	}
+}
---
+++ src/bmp_io/_ue33.java
@@ -0,0 +1,96 @@
+package bmp_io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public final class _ue33 {
+
+	private static void setPixels(BmpImage bmp, PixelColor[][] pixels) {
+		for(int x = 0; x != pixels.length; x++) {
+			for(int y = 0; y != pixels[x].length; y++) {
+				bmp.image.setRgbPixel(x, y, pixels[x][y]); 
+			}
+		}
+	}
+
+	private static PixelColor[][] getPixels(BmpImage bmp) {
+		PixelColor[][] ret = new PixelColor[bmp.image.getWidth()][bmp.image.getHeight()]; 
+		for(int x = 0; x != bmp.image.getWidth(); x++) {
+			for(int y = 0; y != bmp.image.getHeight(); y++) {
+				ret[x][y] = bmp.image.getRgbPixel(x, y); 
+			}
+		}
+		return ret; 
+	}
+	
+	private static PixelColor[][] verticalDownSample(BmpImage source, int pixelsToSample) {
+		// get data 
+		PixelColor[][] data = getPixels(source); 
+		for (int x = 0; x < data.length; x++) {
+			int lastIndex = data[x].length - 1; 
+			for (int y = 0; y < data[x].length; y += pixelsToSample + 1) { 
+				int valueIndex = (y + pixelsToSample) > lastIndex ? lastIndex : y + pixelsToSample; 
+				for (int k = 0; k < pixelsToSample; k++) {
+					int overwriteIndex = y + k; 
+					if (overwriteIndex <= lastIndex) {
+						data[x][y+k] = data[x][valueIndex];
+					}
+				}
+
+			}
+		}
+		return data; 
+	}
+
+	private static PixelColor[][] horizontalDownSample(BmpImage source, int pixelsToSample) {
+		// get data 
+		PixelColor[][] data = getPixels(source); 
+		int lastIndex = data.length - 1; 
+		for (int x = 0; x < data.length; x += pixelsToSample + 1) {
+			for (int y = 0; y < data[x].length; y++) { 
+				int valueIndex = (x + pixelsToSample) > lastIndex ? lastIndex : x + pixelsToSample; 
+				for (int k = 0; k < pixelsToSample; k++) {
+					int overwriteIndex = x + k; 
+					if (overwriteIndex <= lastIndex) {
+						data[x+k][y] = data[valueIndex][y];
+					}
+				}
+
+			}
+		}
+		return data; 
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		
+
+		// enter file name here 
+		String fileName = "media_in/Detail_LSG.bmp";
+		
+		BmpImage bmp = null;
+
+		try {
+			InputStream in = new FileInputStream(fileName);
+			bmp = BmpReader.read_bmp(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		OutputStream out = new FileOutputStream(fileName);
+
+		try {
+			// modify bmp before write_bmp is called
+			setPixels(bmp, horizontalDownSample(bmp, 16));
+			BmpWriter.write_bmp(out, bmp);
+		}catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			out.close();
+		}
+	}
+}
